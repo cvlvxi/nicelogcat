6e7063f1aeeea15d8adb4d47b87df4dba9e8b5cf
cvlvxi <cvlvxi@outlook.com>
2022-03-15 03:49:20 +1100

    nice

--- boxlogs/main.py
+++ boxlogs/main.py
@@ -2,7 +2,9 @@ import os
 from datetime import datetime
 from colorama import init, Fore, Back, Style
 from traceback import print_exc
-from sh import tail, glob
+from sh import tail
+import glob
+import time
 import json
 import argparse
 import itertools
@@ -10,14 +12,12 @@ from pathlib import Path
 import sys
 import signal
 import hashlib
+import subprocess
+import select
 from bs4 import BeautifulSoup
-
-CURR_LINE = ""
-PREV_LINE = ""
 
 curr_dir = Path(__file__).parent
 tmp_dir = curr_dir / "box_tmp_dir"
-curr_line_file = tmp_dir / "currline.txt"
 html_error_file = tmp_dir / "htmlerror"
 logs_dir = "/data/logs"
 html_errors = {}
@@ -27,7 +27,10 @@ cols = {
     "k": Fore.CYAN,
     "v": Fore.WHITE,
     "h": Back.GREEN + Fore.BLACK,
-    "alert": Back.BLACK + Fore.WHITE
+    "alert": Back.BLACK + Fore.WHITE,
+    "info":  Fore.GREEN,
+    "warn": Fore.YELLOW,
+    "error": Fore.RED
 }
 
 
@@ -77,9 +80,7 @@ def style(val, color=None):
 
 def alert(some_str):
     print()
-    print()
     print(style(some_str, cols["alert"]))
-    print()
     print()
 
 
@@ -94,20 +95,13 @@ def flatten_dict(dd, separator='_', prefix=''):
             } if isinstance(dd, dict) else {prefix: dd}
 
 
-def handler(signum, frame):
-    alert("Saving curr_line to: " + str(curr_line_file))
-    with open(str(curr_line_file), "w") as f:
-        f.write(CURR_LINE)
-    sys.exit(1)
-
-
-signal.signal(signal.SIGINT, handler)
-
 parser = argparse.ArgumentParser(description='BoxLogs')
+parser.add_argument('-m', '--show-meta', action="store_true",
+                    help="Show Meta fields")
 parser.add_argument('-c', '--clear', action="store_true",
                     help="Clear Logs")
-parser.add_argument('-n', '--new', action="store_true",
-                    help="Start logging new")
+parser.add_argument('-a', '--all', action="store_true",
+                    help="Start from beginning")
 parser.add_argument('-s', '--no-suspend', action="store_true",
                     help="Don't suspend if highlight hit")
 parser.add_argument('--h', nargs="*", dest="highlight",
@@ -121,21 +115,12 @@ if args.filter:
 if args.highlight:
     args.highlight = flatten(args.highlight)
     alert("Will find these items:" + str(args.highlight))
-if curr_line_file.exists():
-    if args.new and curr_line_file.exists():
-        os.remove(str(curr_line_file))
-    else:
-        with open(str(curr_line_file), "r") as f:
-            PREV_LINE = f.read().strip()
-            if not PREV_LINE:
-                args.new = True
 if args.clear:
     print("Removing Files")
     init_remove_files()
     sys.exit(1)
 
 init(autoreset=True)
-
 
 logdir = "/data/logs/"
 continue_strings = ["next", "n", "c", "continue", "cont", " ", "enter"]
@@ -144,73 +129,104 @@ stop_suspend = ["s", "stop"]
 logs = [os.path.join(logdir, x) for x in os.listdir(logdir)]
 
 
+def line_loop(line):
+    line = line.strip()
+    if not line:
+        return False
+    line_str = ""
+    log_level = ""
+    if line[0] == "{":
+        json_data = json.loads(line)
+        json_data = flatten_dict(json_data)
+        log_level = json_data.pop("level")
+        log_col = cols[log_level] if log_level in cols else Fore.CYAN
+        log_level = style(log_level.upper(), log_col)
+        if not args.show_meta:
+            meta_keys = [k for k in json_data if 'meta' in k]
+            for k in meta_keys:
+                json_data.pop(k)
+        json_str = " ".join(["{}: {}".format(style(k, cols["k"]), style(
+            v, cols["v"])) for k, v in json_data.items()])
+        if contains_html(line):
+            html_error = json_data["error_body"]
+            write_html(html_error)
+            return False
+    else:
+        json_str = line
+    line_str += log_level + ": " + json_str
+
+    # Filters and highlights
+    if args.filter and any([filter_word in line for filter_word in args.filter]):
+        return False
+    found_highlight = False
+    if args.highlight:
+        for highlight_word in args.highlight:
+            if highlight_word in line:
+                found_highlight = True
+                line_str = line_str.replace(highlight_word, style(highlight_word, cols["h"]))
+
+    # Main Print
+    print(line_str)
+
+    # Suspend loop
+    if not args.no_suspend and found_highlight:
+        alert("Type: [' '|'c'|'n'] to go to next highlight or ['s'|'stop'] to stop suspending")
+        suspend_input = ""
+        while True:
+            if any([x == suspend_input for x in stop_suspend]):
+                args.no_suspend = True
+                break
+            if any([x == suspend_input for x in continue_strings]):
+                break
+            suspend_input = input()
+            suspend_input = suspend_input.lower()
+
+
+def print_loop(log_file, log_size_threshold):
+    # alert("Starting print_loop, log_file: " + log_file)
+    log_path = Path(log_file)
+    log_size = 0
+    f = subprocess.Popen(['tail', '-F', log_file],
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = select.poll()
+    p.register(f.stdout)
+    check_size_after_secs = 5
+    start_time = time.time()
+    while True:
+        if p.poll(1):
+            line = f.stdout.readline()
+            will_exit = line_loop(line.decode())
+            if will_exit:
+                return
+        else:
+            curr_time = time.time()
+            if (curr_time - start_time) > check_size_after_secs:
+                log_size = log_path.stat().st_size
+                if log_size > log_size_threshold:
+                    return
+
+
 def main_loop():
-    global CURR_LINE
-    global PREV_LINE
-    if PREV_LINE:
-        alert("Will resume from previous state. Specify -n to start fresh")
-    start_logging = False
-    start_logging_limit = 10000
-    count = 0
-    for line in tail("-f", glob("/data/logs/*"),  _iter=True):
-        CURR_LINE = line
-        if not start_logging and not args.new and PREV_LINE:
-            start_logging = PREV_LINE in CURR_LINE
-        else:
-            start_logging = True
-        if count >= start_logging_limit:
-            start_logging = True
-        if not start_logging:
-            print(count)
-            continue
-        now = datetime.now()
-        line = line.strip()
-        if not line:
-            continue
-        line_str = ""
-        line_str += style(now.ctime(), cols["time"])
-        if line[0] == "{":
-            json_data = json.loads(line)
-            json_data = flatten_dict(json_data)
-            json_str = " ".join(["{}: {}".format(style(k, cols["k"]), style(
-                v, cols["v"])) for k, v in json_data.items()])
-            if contains_html(line):
-                html_error = json_data["error_body"]
-                write_html(html_error)
-                continue
-        else:
-            json_str = line
-        line_str += " " + json_str
+    log_size_threshold_1mb = 1000000
+    log_files = list(
+        filter(os.path.isfile, glob.glob(os.path.join(logdir, "*"))))
+    log_files.sort(key=lambda x: os.path.getmtime(x))
 
-        # Main Print
-        print(line_str)
+    prev_log_files = log_files[:-1]
 
-        # Filters and highlights
-        if args.filter and any([filter_word in line for filter_word in args.filter]):
-            continue
-        found_highlight = False
-        if args.highlight:
-            for highlight_word in args.highlight:
-                if highlight_word in line:
-                    found_highlight = True
-                    line_str = line_str.replace(
-                        highlight_word, style(highlight_word, cols["h"]))
-        # Suspend loop
-        if not args.no_suspend and found_highlight:
+    if args.all:
+        for log_file in prev_log_files:
+            with open(log_file, "r") as f:
+                prev_logs = f.read()
+                for line in prev_logs.split("\n"):
+                    line_loop(line)
 
-            alert("Type one of: " + ",".join(continue_strings))
-            alert("Type one of: " + ",".join(stop_suspend) + " to stop suspending")
-
-            suspend_input = ""
-            while True:
-                if any([x == suspend_input for x in stop_suspend]):
-                    args.no_suspend = True
-                    break
-                if any([x == suspend_input for x in continue_strings]):
-                    break
-                suspend_input = input()
-                suspend_input = suspend_input.lower()
-        count += 1
+    while True:
+        log_files = list(
+            filter(os.path.isfile, glob.glob(os.path.join(logdir, "*"))))
+        log_files.sort(key=lambda x: os.path.getmtime(x))
+        log_file = log_files[-1]
+        print_loop(log_file, log_size_threshold_1mb)
 
 
 if __name__ == "__main__":
